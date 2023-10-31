6f0bcde7c0541ebbeab9459f04cfaee79f9abb30
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-10-31 13:08:33 -0600

    Sumary

--- REACT-PRYECT/caezpagosalumnos/src/components/cards.jsx
+++ REACT-PRYECT/caezpagosalumnos/src/components/cards.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import style from './cards.module.css'
-
+/**
+ * Este es un componente card, el cual debera tener un diseo predeterminado con parametros especificos que se brindaran desde las etiquetas como las porps
+ */
 export default function cards() {
     return (
         <div>
@@ -10,7 +12,6 @@ export default function cards() {
                         <div className={style.numbers}>1,504</div>
                         <div class={style.cardName}>Daily Views</div>
                     </div>
-
                     <div className={style.iconBx}>
                         <ion-icon name="eye-outline"></ion-icon>
                     </div>
