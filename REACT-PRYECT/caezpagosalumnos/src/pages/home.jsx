68a236751d19a635c39fbf3955bf53c669fb1eaf
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-11-03 14:29:54 -0600

    agagar tarjetas

--- REACT-PRYECT/caezpagosalumnos/src/pages/home.jsx
+++ REACT-PRYECT/caezpagosalumnos/src/pages/home.jsx
@@ -8,9 +8,16 @@ export default function home() {
                 saldo={10010}
                 titulo="saldo"
             ></Cards>
+
+            <Cards
+                saldo={50}
+                titulo="Pendientes">
+            </Cards>
+            <Cards saldo={75}
+                titulo="Pagados">
+            </Cards>
         </div>
     )
 }
-
 
 //cosas que no se que son
