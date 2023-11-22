411d85e43ba5e782bcf5ae352de99c9469fe17c7
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-11-22 16:42:22 -0600

    cambios de la estructura jsx

--- REACT-PRYECT/caezpagosalumnos/src/App.js
+++ REACT-PRYECT/caezpagosalumnos/src/App.js
@@ -84,7 +84,7 @@ function App() {
   ]
   return (
     <div className="contenedor">
-      <div class="navigation">
+      <div className="navigation">
         <ul>
           <li className= "top">
             <a href="#">
@@ -109,6 +109,12 @@ function App() {
       </div>
       <div className='head'>
         <h2>Buscador</h2>
+        {/* Agregar el icono del menú para pantallas más pequeñas */}
+        <div className="menu-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+          </svg>
+        </div>
       </div>
       <div className='Contenido'>
         <Home></Home>
