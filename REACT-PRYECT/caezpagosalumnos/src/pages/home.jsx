03bab07f55b6deff3d7f0a7b076aacce699e3bff
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-15 12:13:03 -0600

    oooo

--- REACT-PRYECT/caezpagosalumnos/src/pages/home.jsx
+++ REACT-PRYECT/caezpagosalumnos/src/pages/home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import style from './Home.module.css'
 import { Cards } from '../components/cards';
 import { Table } from '../components/Table';
+import RegistroAlumno from '../components/RegistroAlumno';
 export default function home() {
     return (
         <div className={style.Home}>
-            <Cards
+            {/* <Cards
                 saldo={10010}
                 titulo="saldo"
                 icono= <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
@@ -15,7 +16,8 @@ export default function home() {
                     <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z" />
                 </svg>
             ></Cards>
-            <Table />
+            <Table /> */}
+            <RegistroAlumno></RegistroAlumno>
         </div>
     )
 }
