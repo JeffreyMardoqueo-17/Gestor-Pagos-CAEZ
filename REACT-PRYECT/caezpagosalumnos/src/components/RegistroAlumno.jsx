defb07f1cff1e950809aa828411ef36e31ec13ff
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-23 18:32:00 -0600

    fin de la historia

--- REACT-PRYECT/caezpagosalumnos/src/components/RegistroAlumno.jsx
+++ REACT-PRYECT/caezpagosalumnos/src/components/RegistroAlumno.jsx
@@ -3,36 +3,53 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 // Import specific Bootstrap components if needed
 import './RegistroAlumno.module.css'
 
+/*
+CREATE TABLE Encargado(
+Id BIGINT NOT NULL PRIMARY KEY IDENTITY (1,1),
+Nombre VARCHAR (50) NOT NULL,
+Apellido VARCHAR (50) NOT NULL,
+IdTipoDoc TINYINT NOT NULL FOREIGN KEY REFERENCES TipoDocumento(Id),
+NumeroDocumento VARCHAR(50) NOT NULL,
+Telefono VARCHAR(50) NOT NULL,
+Direccion TINYINT NOT NULL FOREIGN KEY REFERENCES Direccion(Id),
+Parentezco VARCHAR (50)
+)
+GO
+----Tabla Alumno
+CREATE TABLE Estudiante(
+Id INT NOT NULL PRIMARY KEY IDENTITY (1,1),
+Nombre VARCHAR (50) NOT NULL,
+Apellido VARCHAR (50) NOT NULL,
+IdGrado TINYINT NOT NULL FOREIGN KEY REFERENCES Grado(Id),
+IdTipoDoc TINYINT NOT NULL FOREIGN KEY REFERENCES TipoDocumento(Id),
+NumeroDocumento VARCHAR(50) NOT NULL,
+IdEncargado BIGINT NOT NULL FOREIGN KEY REFERENCES Encargado(Id)
+)*/
 const RegistroAlumno = () => {
     return (
         <div className='formulario'>
             <form className="row g-3">
+                {/* //Nombre */}
                 <div className="col-md-6">
-                    <label htmlFor="inputEmail4" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="inputEmail4" />
+                    <label htmlFor="inputName" className="form-label">Nombre</label>
+                    <input type="text" className="form-control" id="inputName" />
                 </div>
+                {/* //Apellido */}
                 <div className="col-md-6">
-                    <label htmlFor="inputPassword4" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="inputPassword4" />
+                    <label htmlFor="inputApellido" className="form-label">Apellido</label>
+                    <input type="text" className="form-control" id="inputApellido" />
                 </div>
-                <div className="col-12">
-                    <label htmlFor="inputAddress" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
-                </div>
-                <div className="col-12">
-                    <label htmlFor="inputAddress2" className="form-label">Address 2</label>
-                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
-                </div>
-                <div className="col-md-6">
-                    <label htmlFor="inputCity" className="form-label">City</label>
-                    <input type="text" className="form-control" id="inputCity" />
-                </div>
+                {/* //Grado */}
                 <div className="col-md-4">
                     <label htmlFor="inputState" className="form-label">State</label>
                     <select id="inputState" className="form-select">
                         <option defaultValue>Choose...</option>
                         <option>...</option>
                     </select>
+                </div>
+                <div className="col-md-6">
+                    <label htmlFor="inputCity" className="form-label">City</label>
+                    <input type="text" className="form-control" id="inputCity" />
                 </div>
                 <div className="col-md-2">
                     <label htmlFor="inputZip" className="form-label">Zip</label>
